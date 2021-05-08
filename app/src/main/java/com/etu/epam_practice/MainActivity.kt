0520b91c05991ecdb4288054f3e6bdf625a1d554
FireRalin <kurralin@mail.ru>
2021-05-08 17:48:26 +0300

    addede binding & buttons listeners

--- app/src/main/java/com/etu/epam_practice/MainActivity.kt
+++ app/src/main/java/com/etu/epam_practice/MainActivity.kt
@@ -2,10 +2,31 @@ package com.etu.epam_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.etu.epam_practice.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.stopStarButton.setOnClickListener{
+            val temp = null
+        }
+
+        binding.previosButton.setOnClickListener{
+            val temp = null
+        }
+
+        binding.nextButton.setOnClickListener{
+            val temp = null
+        }
     }
 }

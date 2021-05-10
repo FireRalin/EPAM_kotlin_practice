c4367736e0c451d7db43ac92e768e02062aed227
FireRalin <kurralin@mail.ru>
2021-05-10 04:13:40 +0300

    played 1 song with seekbar

--- app/src/main/java/com/etu/epam_practice/MainActivity.kt
+++ app/src/main/java/com/etu/epam_practice/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.etu.epam_practice
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.SeekBar
 import com.etu.epam_practice.databinding.ActivityMainBinding
 
 
@@ -9,16 +12,31 @@ import com.etu.epam_practice.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var runnable: Runnable
+    private var handler = Handler()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val mediaplayer = MediaPlayer.create(this,R.raw.morg)
+
+        binding.seekbar.progress = 0
+        binding.seekbar.max = mediaplayer.duration
+
 
 
         binding.stopStarButton.setOnClickListener{
             val temp = null
+            if (!mediaplayer.isPlaying){
+                mediaplayer.start()
+                binding.stopStarButton.setImageResource(R.drawable.ic_baseline_pause_40)
+            }else{
+                mediaplayer.pause()
+                binding.stopStarButton.setImageResource(R.drawable.ic_baseline_play_arrow_40)
+            }
         }
 
         binding.previosButton.setOnClickListener{
@@ -28,5 +46,35 @@ class MainActivity : AppCompatActivity() {
         binding.nextButton.setOnClickListener{
             val temp = null
         }
+
+        binding.seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, pos: Int, changed: Boolean) {
+                if(changed){
+                    mediaplayer.seekTo(pos)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        runnable = Runnable {
+            binding.seekbar.progress = mediaplayer.currentPosition
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+
+        mediaplayer.setOnCompletionListener {
+            binding.stopStarButton.setImageResource(R.drawable.ic_baseline_play_arrow_40)
+            binding.seekbar.progress = 0
+        }
+
+
+
     }
 }
